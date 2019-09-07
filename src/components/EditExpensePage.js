1dfcf9ca1534de589f550cea7adae99684648a36
dsteoh <desiangteoh@gmail.com>
2019-09-07 18:24:27 +1000

    Implemented edit/update expense on firebase DB

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpenese, removeExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.editExpenese(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
 
     onClick = () => {
-        this.props.removeExpense({id: this.props.expense.id});
+        this.props.startRemoveExpense({id: this.props.expense.id});
         this.props.history.push('/');
     };
 
@@ -29,8 +29,8 @@ export class EditExpensePage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch, props) => ({
-    editExpenese: (id, expense) => dispatch(editExpenese(id, expense)),
-    removeExpense: (data) => dispatch(removeExpense(data))
+    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
+    startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
 const mapStateToProps = (state, props) => {
